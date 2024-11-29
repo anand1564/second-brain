e93822f048e4a79beed515ad7a74bb7718b02441
Anand Pandey <anand8584pandey@gmail.com>
2024-11-29 22:16:45 +0530

    Initialised frontend with tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-center mt-10'>Hello from Frontend</h1>
+    </div>
+  )
+}
+
+export default App
